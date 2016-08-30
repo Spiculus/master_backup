6a0067f1c1855cc2c71423fbcd2744b6e86a79a1
Jorge L Diaz <Splinter1021@users.noreply.github.com>
2016-08-30 18:36:31 -0400

    Update backup-push.sh

--- backup-push.sh
+++ backup-push.sh
@@ -1,6 +1,6 @@
 # !/bash/bin
 # This script will create a backup, encrypt it, copy it into a working
-# repository, and then push it to the remote repository.
+# repository, and then push it to the remote repository using lfs to handle larger files.
 
 echo "Creating backup"
 sudo tar -cvpzf backup.tar.gz /home/splinter/Pictures /home/splinter/Downloads /home/splinter/scripts /home/splinter/TestJavas /home/splinter/Videos
@@ -13,4 +13,6 @@ sudo git init
 sudo git lfs track backup.tar.gz.cpt
 sudo git add backup.tar.gz.cpt
 sudo git commit -m bakup.tar.gz.cpt
-sudo git push origin master
+sudo git push origin master 
+# you will get a prompt to enter username and password. I believe you can avoid this by cloning the remote repsitory directly,
+# or using 'git config' options but I am not 100%, will update.
